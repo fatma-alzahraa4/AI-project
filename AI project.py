b9f5ebdfa08ec334499831064cbf22fa5d102ec0
fatma-alzahraa4 <Kfatmaalzahraa@gmail.com>
2023-05-19 20:05:02 +0300

    game over

--- AI project.py
+++ AI project.py
@@ -189,20 +189,120 @@ def alpha_beta_pruning(board, depth, alpha, beta, maximizingPlayer):
                 break
         return column, value
     else: # Minimizing player
-                  value = math.inf
-                  column = random.choice(valid_locations)
-                  for col in valid_locations:
-                   row = get_next_open_row(board, col)
-                   b_copy = board.copy()
-                  drop_piece(b_copy, row, col, PLAYER_PIECE)
-                  new_score = alpha_beta_pruning(b_copy, depth-1, alpha, beta, True)[1]
-                  if new_score < value:
-                   value = new_score
-                   column = col
-                  beta = min(beta, value)
-                  if alpha >= beta:
-                      break
-     return column, value
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER_PIECE)
+            new_score = alpha_beta_pruning(b_copy, depth-1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
+    
+
+def get_valid_locations(board):
+	valid_locations = []
+	for col in range(COLUMN_COUNT):
+		if is_valid_location(board, col):
+			valid_locations.append(col)
+	return valid_locations
 
 
+def pick_best_move(board, piece):
+
+	valid_locations = get_valid_locations(board)
+	best_score = -10000
+	best_col = random.choice(valid_locations)
+	for col in valid_locations:
+		row = get_next_open_row(board, col)
+		temp_board = board.copy()
+		drop_piece(temp_board, row, col, piece)
+		score = score_position(temp_board, piece)
+		if score > best_score:
+			best_score = score
+			best_col = col
+
+	return best_col      
+board = create_board()
+print_board(board)
+game_over = False
+turn = random.randint(PLAYER, AI)
+
+
+while not game_over:
+     
+	 # # Ask for Player 1 Input
+    
+	if turn == PLAYER and not game_over:
+		start_time = time.time()				
+        
+		
+		print("player1 play")
+		
+        #calling the algorithms
+		###### col, minimax_score = alpha_beta_pruning(board, 5, -math.inf, math.inf, True)
+		col, minimax_score = minimax(board, 5, True)
+		
+		if is_valid_location(board, col):
+			
+			row = get_next_open_row(board, col)
+			drop_piece(board, row, col, PLAYER_PIECE)
+
+			if winning_move(board, PLAYER_PIECE):
+				print("Player1 wins!!")
+				game_over = True
+
+			print_board(board)
+			
+			turn += 1
+			turn = turn % 2
+			end_time = time.time()
+		player1_times.append(end_time - start_time)
+
+
+	
+    # # Ask for Player 2 Input
+	if turn == AI and not game_over:
+		start_time = time.time()				
+		print("player2 play")
+		
+
+		#calling the algorithms
+		col, minimax_score = alpha_beta_pruning(board, 5, -math.inf, math.inf, True)
+		###### col, minimax_score = minimax(board, 5, True)
+
+
+		if is_valid_location(board, col):
+			
+			row = get_next_open_row(board, col)
+			drop_piece(board, row, col, AI_PIECE)
+
+			if winning_move(board, AI_PIECE):
+				print("Player 2 wins!!")
+				game_over = True
+
+			print_board(board)
+			
+			turn += 1
+			turn = turn % 2
+			end_time = time.time()
+		player2_times.append(end_time - start_time)
+			
+
+
+
+
+
+
+plt.plot(player1_times, label='Player 1 (Minimax)')
+plt.plot(player2_times, label='Player 2 (Alpha-Beta Pruning)')
+plt.xlabel('Move')
+plt.ylabel('Time (seconds)')
+plt.legend()
+plt.show()
 
